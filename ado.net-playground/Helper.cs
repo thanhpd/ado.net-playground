f1d72c9707e69b4ae796afe09218ddc860a912c1
Thanh Phan <thanh.phan@niteco.se>
2016-11-09 14:03:35 +0700

    First commit

---
+++ ado.net-playground/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ado.net_playground
+{
+    public class Helper
+    {
+        // Helper classes go here
+    }
+}
---
+++ ado.net-playground/WorkerRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ado.net_playground
+{
+    class WorkerRepo
+    {
+        public static void EstablishConnection()
+        {
+            string connectionString = ConfigurationManager.AppSettings["NorthwindConnectionString"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+            }
+        }
+    }
+}
